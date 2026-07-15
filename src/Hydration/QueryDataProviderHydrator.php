4a24ba0e5a80cd2ec8c9f8968e0b507b92950e08
Степанцев Альберт <albert.stepantsev@sprintf.ru>
2026-07-15 12:47:45 +0300

    + Разработка провайдеров данных

--- src/Hydration/QueryDataProviderHydrator.php
+++ src/Hydration/QueryDataProviderHydrator.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query;
 use SprintF\Bundle\EntityTable\DataProvider\EntityTableDataProviderInterface;
 use SprintF\Bundle\EntityTable\DataProvider\QueryDataProvider;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\UX\LiveComponent\Hydration\HydrationExtensionInterface;
 
 /**
  * Сервис гидрации-дегидрации дата-провайдеров на основе объекта класса ORM\Query.
@@ -16,6 +18,8 @@ class QueryDataProviderHydrator implements DataProviderHydratorInterface
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
+        #[Autowire(service: 'ux.live_component.doctrine_entity_hydration_extension')]
+        private readonly HydrationExtensionInterface $doctrineEntityHydration,
     ) {
     }
 
@@ -26,9 +30,19 @@ class QueryDataProviderHydrator implements DataProviderHydratorInterface
 
     public function hydrate(array|string $value): QueryDataProvider
     {
+        $p = $value['query']['params'] ?? [];
+        $params = [];
+        foreach ($p as $key => $param) {
+            if (isset($param['name'], $param['class']) && $this->doctrineEntityHydration->supports($param['class'])) {
+                $params[$param['name']] = $this->doctrineEntityHydration->hydrate($param['id'], $param['class']);
+            } else {
+                $params[$key] = $param;
+            }
+        }
+        
         $query = new Query($this->entityManager)
             ->setDQL($value['query']['dql'])
-            ->setParameters($value['query']['params'])
+            ->setParameters($params);
         ;
 
         return new QueryDataProvider($query);
@@ -39,7 +53,16 @@ class QueryDataProviderHydrator implements DataProviderHydratorInterface
         $ret = ['class' => QueryDataProvider::class];
         /* @var QueryDataProvider $object */
         $ret['query']['dql'] = $object->getQuery()->getDQL();
-        $ret['query']['params'] = $object->getQuery()->getParameters()->toArray();
+        $params = $object->getQuery()->getParameters()->toArray();
+        foreach ($params as $key => $param) {
+            if ($this->doctrineEntityHydration->supports(get_class($param->getValue()))) {
+                $ret['query']['params'][$key]['name'] = $param->getName();
+                $ret['query']['params'][$key]['class'] = get_class($param->getValue());
+                $ret['query']['params'][$key]['id'] = $this->doctrineEntityHydration->dehydrate($param->getValue());
+            } else {
+                $ret['query']['params'][$key] = $param;
+            }
+        }
 
         return $ret;
     }
