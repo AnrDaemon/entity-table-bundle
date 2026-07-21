7670fdd28d298f46a607e3e59d57ba1630c886ea
Степанцев Альберт <albert.stepantsev@sprintf.ru>
2026-07-21 18:19:55 +0300

    + Разработка провайдеров данных

--- src/Hydration/QueryBuilderDataProviderHydrator.php
+++ src/Hydration/QueryBuilderDataProviderHydrator.php
@@ -6,6 +6,8 @@ namespace SprintF\Bundle\EntityTable\Hydration;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Parameter;
+use Doctrine\ORM\QueryBuilder;
 use SprintF\Bundle\EntityTable\DataProvider\EntityTableDataProviderInterface;
 use SprintF\Bundle\EntityTable\DataProvider\QueryBuilderDataProvider;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -25,7 +27,6 @@ class QueryBuilderDataProviderHydrator implements DataProviderHydratorInterface
         private readonly HydrationExtensionInterface $doctrineEntityHydration,
         #[AutowireIterator(tag: 'serializer.normalizer')]
         private readonly iterable $normalizers,
-        private readonly DenormalizerInterface&NormalizerInterface $serializer,
     ) {
     }
 
@@ -34,73 +35,9 @@ class QueryBuilderDataProviderHydrator implements DataProviderHydratorInterface
         return QueryBuilderDataProvider::class === $className;
     }
 
-    public function hydrate(array|string $value): QueryBuilderDataProvider
-    {
-        $qb = new \Doctrine\ORM\QueryBuilder($this->entityManager);
-
-        foreach ($value['builder']['parts'] as $name => $part) {
-            if (null === $part) {
-                continue;
-            }
-            switch ($name) {
-                case 'distinct':
-                    $qb->distinct($part);
-                    break;
-                case 'select':
-                    foreach ($part as $p) {
-                        $qb->addSelect($p['parts']);
-                    }
-                    break;
-                case 'from':
-                    foreach ($part as $p) {
-                        $qb->from($p['from'], $p['alias'], $p['indexBy']);
-                    }
-                    break;
-                case 'join':
-                    foreach ($part as $root => $joins) {
-                        foreach ($joins as $join) {
-                            switch ($join['joinType']) {
-                                case 'INNER':
-                                    $qb->innerJoin($join['join'], $join['alias'], $join['conditionType'], $join['condition'], $join['indexBy']);
-                                    break;
-                                case 'LEFT':
-                                    $qb->leftJoin($join['join'], $join['alias'], $join['conditionType'], $join['condition'], $join['indexBy']);
-                                    break;
-                            }
-                        }
-                    }
-                    break;
-                case 'orderBy':
-                    foreach ($part as $p) {
-                        foreach ($p['parts'] as $order) {
-                            $qb->addOrderBy(...explode(' ', $order));
-                        }
-                    }
-                    break;
-            }
-        }
-
-        $p = $value['builder']['params'] ?? [];
-        $params = [];
-        foreach ($p as $key => $param) {
-            if (isset($param['name'], $param['class'], $param['id']) && $this->doctrineEntityHydration->supports($param['class'])) {
-                $params[$param['name']] = $this->doctrineEntityHydration->hydrate($param['id'], $param['class']);
-            } elseif (isset($param['name'], $param['class'], $param['value'])) {
-                foreach ($this->normalizers as $normalizer) {
-                    if ($normalizer instanceof DenormalizerInterface && $normalizer->supportsDenormalization($param['value'], $param['class'])) {
-                        $params[$param['name']] = $normalizer->denormalize($param['value'], $param['class']);
-                        break 2;
-                    }
-                }
-            }
-
-            $params[$key] = $param;
-        }
-
-        $qb->setParameters(new ArrayCollection($params));
-
-        return new QueryBuilderDataProvider($qb);
-    }
+    // ----
+    //  Дегидрация
+    // ----
 
     public function dehydrate(EntityTableDataProviderInterface $object): array
     {
@@ -109,29 +46,215 @@ class QueryBuilderDataProviderHydrator implements DataProviderHydratorInterface
         /* @var QueryBuilderDataProvider $object */
         $builder = $object->getQueryBuilder();
 
-        $parts = $builder->getDQLParts();
-        $ret['builder']['parts'] = $this->serializer->normalize($parts);
+        $ret['builder']['object'] = iterator_to_array($this->dehydrateObject(
+            $builder,
+            exlcude: ['dql', 'parameters', 'em']
+        ));
+        $ret['builder']['params'] = $this->dehydrateQueryBuilderParams($builder);
 
-        $params = $builder->getParameters()->toArray();
-        foreach ($params as $key => $param) {
-            if (is_object($param->getValue()) && $this->doctrineEntityHydration->supports(get_class($param->getValue()))) {
-                $ret['builder']['params'][$key]['name'] = $param->getName();
-                $ret['builder']['params'][$key]['class'] = get_class($param->getValue());
-                $ret['builder']['params'][$key]['id'] = $this->doctrineEntityHydration->dehydrate($param->getValue());
-            } elseif (is_object($param->getValue())) {
-                foreach ($this->normalizers as $normalizer) {
-                    if ($normalizer instanceof NormalizerInterface && $normalizer->supportsNormalization($param->getValue())) {
-                        $ret['builder']['params'][$key]['name'] = $param->getName();
-                        $ret['builder']['params'][$key]['class'] = get_class($param->getValue());
-                        $ret['builder']['params'][$key]['value'] = $normalizer->normalize($param->getValue());
-                        break 2;
-                    }
+        return $ret;
+    }
+
+    private function dehydrateObject(object $object, array $properties = [], array $exlcude = []): iterable
+    {
+        if (empty($properties)) {
+            $reflection = new \ReflectionObject($object);
+            $props = $reflection->getProperties(~\ReflectionProperty::IS_STATIC);
+            foreach ($props as $prop) {
+                if (!in_array($prop->getName(), $exlcude)) {
+                    $properties[] = $prop->getName();
                 }
-
-                $ret['builder']['params'][$key] = $param;
             }
         }
 
+        foreach ($properties as $property) {
+            if (in_array($property, $exlcude)) {
+                continue;
+            }
+            try {
+                $reflectionProperty = new \ReflectionProperty($object, $property);
+                if ($reflectionProperty->isStatic() || !$reflectionProperty->isInitialized($object)) {
+                    continue;
+                }
+                $value = $reflectionProperty->getValue($object);
+
+                if (is_object($value) || is_array($value)) {
+                    $value = iterator_to_array($this->dehydrateValue($value));
+                }
+
+                yield $property => $value;
+            } catch (\ReflectionException) {
+                continue;
+            }
+
+            yield '@class' => get_class($object);
+        }
+    }
+
+    public function dehydrateValue(mixed $value): iterable
+    {
+        if (\is_object($value)) {
+            yield from $this->dehydrateObject($value);
+        } elseif (\is_array($value)) {
+            yield from $this->dehydrateArray($value);
+        }
+    }
+
+    public function dehydrateArray(array $values): iterable
+    {
+        foreach ($values as $key => $value) {
+            if (is_object($value) || is_array($value)) {
+                $value = iterator_to_array($this->dehydrateValue($value));
+            }
+            yield $key => $value;
+        }
+    }
+
+    // ----
+    //  Гидрация
+    // ----
+
+    public function hydrate(array|string $value): QueryBuilderDataProvider
+    {
+        $qb = new QueryBuilder($this->entityManager);
+        $qb = $this->hydrateObject($value['builder']['object'], $qb);
+
+        $qb->setParameters($this->hydrateQueryBuilderParams($value['builder']['params'] ?? []));
+
+        return new QueryBuilderDataProvider($qb);
+    }
+
+    private function hydrateObject(array $data, ?object $object = null): object
+    {
+        if (null === $object) {
+            $class = $data['@class'] ?? throw new \InvalidArgumentException('Class for object is not set');
+            class_exists($class) or throw new \InvalidArgumentException('Class for object does not exist');
+
+            try {
+                $reflection = new \ReflectionClass($class);
+
+                if ($reflection->isEnum()) {
+                    return $class::{$data['name']};
+                }
+
+                $arguments = [];
+                $constructor = $reflection->getConstructor();
+                if ($constructor->getNumberOfRequiredParameters() > 0) {
+                    $allParameters = $constructor->getParameters();
+                    foreach ($allParameters as $constructorParameter) {
+                        if ($constructorParameter->isPromoted() && !$constructorParameter->isOptional()) {
+                            $name = $constructorParameter->getName();
+                            if (isset($data[$name])) {
+                                $arguments[$name] = $data[$name];
+                            } else {
+                                throw new \InvalidArgumentException('Invalid constructor parameter value for: '.$name);
+                            }
+                        }
+                    }
+                }
+
+                $object = new $class(...$arguments);
+            } catch (\Throwable $exception) {
+                throw $exception;
+            }
+
+            unset($data['@class']);
+        }
+
+        foreach ($data as $property => $value) {
+            try {
+                if (is_array($value)) {
+                    if (!empty($value['@class'])) {
+                        $value = $this->hydrateObject($value);
+                    } else {
+                        $value = $this->hydrateArray($value);
+                    }
+                }
+                $reflectionProperty = new \ReflectionProperty($object, $property);
+                $reflectionProperty->setValue($object, $value);
+            } catch (\ReflectionException) {
+                continue;
+            }
+        }
+
+        return $object;
+    }
+
+    private function hydrateArray(array $values): array
+    {
+        $ret = [];
+        foreach ($values as $k => $value) {
+            if (is_array($value)) {
+                if (!empty($value['@class'])) {
+                    $value = $this->hydrateObject($value);
+                } else {
+                    $value = $this->hydrateArray($value);
+                }
+            }
+            $ret[$k] = $value;
+        }
+
         return $ret;
+    }
+
+    // ----
+    //  Параметры
+    // ----
+
+    /**
+     * Специальный метод для дегидрации параметров.
+     * Некоторые из них могут быть сущностями Doctrine, и тогда мы сохраняем только идентификатор такой сущности.
+     * Для иных объектов сохраняем класс.
+     */
+    private function dehydrateQueryBuilderParams(QueryBuilder $builder): array
+    {
+        $p = [];
+        $params = $builder->getParameters()->toArray();
+
+        foreach ($params as $key => $param) {
+            $p[$key]['name'] = $param->getName();
+            $p[$key]['type'] = $param->getType();
+            $value = $param->getValue();
+
+            if (is_object($value)) {
+                $p[$key]['@class'] = get_class($value);
+                if ($this->doctrineEntityHydration->supports(get_class($value))) {
+                    $p[$key]['id'] = $this->doctrineEntityHydration->dehydrate($value);
+                } else {
+                    foreach ($this->normalizers as $normalizer) {
+                        if ($normalizer instanceof NormalizerInterface && $normalizer->supportsNormalization($value)) {
+                            $p[$key]['value'] = $normalizer->normalize($value);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                $p[$key]['value'] = $value;
+            }
+        }
+
+        return $p;
+    }
+
+    private function hydrateQueryBuilderParams(array $params): ArrayCollection
+    {
+        $p = [];
+
+        foreach ($params as $key => $param) {
+            if (isset($param['name'], $param['@class'], $param['id']) && $this->doctrineEntityHydration->supports($param['@class'])) {
+                $p[] = new Parameter($param['name'], $this->doctrineEntityHydration->hydrate($param['id'], $param['@class']));
+            } elseif (isset($param['name'], $param['@class'], $param['value'])) {
+                foreach ($this->normalizers as $normalizer) {
+                    if ($normalizer instanceof DenormalizerInterface && $normalizer->supportsDenormalization($param['value'], $param['@class'])) {
+                        $p[] = new Parameter($param['name'], $normalizer->denormalize($param['value'], $param['@class']));
+                        continue 2;
+                    }
+                }
+            } else {
+                $p[] = new Parameter($param['name'], $param['value'], $param['type']);
+            }
+        }
+
+        return new ArrayCollection($p);
     }
 }
